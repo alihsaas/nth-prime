32e74cfd9aa98d4c9ae043ac4c05fdcb9a7a5f5c
alihsaas <alihsaas2@gmail.com>
2021-05-19 08:59:58 +0300

    update benchmark

--- benches/my_benchmark.rs
+++ benches/my_benchmark.rs
@@ -1,7 +1,7 @@
 use criterion::{black_box, criterion_group, criterion_main, Criterion};
 
 fn criterion_benchmark(c: &mut Criterion) {
-    c.bench_function("100000th prime", |b| b.iter(|| nth_prime::get_nth_prime(black_box(100000))));
+    c.bench_function("500000th prime", |b| b.iter(|| nth_prime::get_nth_prime(black_box(500000))));
 }
 
 criterion_group!(benches, criterion_benchmark);
